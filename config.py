5df5ba8178d8b0c719490d6b0b7bef2018fa87fb
Behnam RK <Hyper.firstblood@gmail.com>
2017-11-18 23:57:45 +0330

    just nothing! :)

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ db_name = 'Cinema_Schedule.sqlite3'
 db_engine_url = '{0}:///{1}'.format(db_system,
                                     db_name)
 
-token = '253839357:AAEEIBG12q9AEgkPcpRM_UdU4l2UCftW6YY'  # QozBot
+token = '253839357:AAEEIBG12q9AEgkPcpRM_UdU4l2UCftW6Ys'  # QozBot
